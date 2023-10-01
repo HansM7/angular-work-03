2ddb9f56eaa2ff7967b0593f285a12f0a32fcf70
HansM7 <melchorhans5@gmail.com>
2023-10-01 12:29:41 -0500

    changue id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
 import { Component } from '@angular/core';
-import { products } from './db/product.db';
-import { IProduct, IStore } from './interfaces/product.interface';
-// import {MatCardModule} from '@angular/material/card'
-
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-	// cartShopping: IStore[] = []
-
-	// captureEmitter(event: object): void {
-	// 	this.cartShopping.push(event as IStore)
-	// 	console.log(this.cartShopping);
-	// }
-
 
 }
